ddaef7a073eb9ee0f56ebf70aaa376510c78c79f
Andrew Cousineau <andruc@gmail.com>
2026-07-14 23:37:23 -0400

    feat: main entry point, wire everything together

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"radio/internal/station"
+	"radio/internal/web"
+)
 
 func main() {
-	fmt.Println("radio server starting...")
+	configPath := flag.String("config", "config.yaml", "path to config file")
+	flag.Parse()
+
+	mgr, err := station.NewManager(*configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	handler := web.NewHandler(mgr, mgr.Port(), mgr.MusicDir())
+
+	mux := http.NewServeMux()
+	mux.Handle("/", handler)
+
+	for _, st := range mgr.AllStations() {
+		s := mgr.StreamerFor(st.Name())
+		if s != nil {
+			mux.Handle("/stream"+st.Mount(), s)
+		}
+	}
+
+	addr := fmt.Sprintf(":%d", mgr.Port())
+	log.Printf("Radio server starting on http://localhost%s", addr)
+	for _, st := range mgr.AllStations() {
+		log.Printf("  %s: http://localhost%s/stream%s", st.Name(), addr, st.Mount())
+	}
+
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
 }
